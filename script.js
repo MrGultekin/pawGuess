5d3e366813e7348eac8db2b053d5059cbd9e9864
MrGultekin <gultekin.de@gmail.com>
2022-03-22 21:56:30 +0100

    script.jsl updated

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 "use strict";
-
-// Speech Recognition
 const msgEl = document.getElementById("msg");
+const articleEl = document.getElementById("article");
+const numberEl = document.querySelector(".number");
+const containerEl = document.querySelector(".container");
+const leftEl = document.querySelector(".left");
+const btnAgainManualEl = document.querySelector(".manual-btn");
+const voiceBtn = document.querySelector(".btn-voice");
+const manualBtn = document.querySelector(".btn-manual");
+const btnContainer = document.querySelector("#btn-container");
+const scoreEl = document.querySelector(".score");
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+let highScore = 0;
+let num;
 
 // window.SpeechRecognition =
 //   window.SpeechRecognition || window.webkitSpeechRecognition;
 
+// Speech Recognition
 window.SpeechRecognition =
   window.webkitSpeechRecognition ||
   window.mozSpeechRecognition ||
@@ -14,9 +27,6 @@ window.SpeechRecognition =
   window.SpeechRecognition;
 
 let recognition = new window.SpeechRecognition();
-
-// Start recognition and game
-recognition.start();
 
 // Capture user speak
 function onSpeak(e) {
@@ -29,14 +39,14 @@ function onSpeak(e) {
 // Write what user speaks
 function writeMessage(voiceMsg) {
   msgEl.innerHTML = `
-    <div>You said: </div>
+    <div class="user-speech">You said: </div>
     <span class="box">${voiceMsg}</span>
   `;
 }
 
 // Check msg against number
 function checkNumber(voiceMsg) {
-  const num = +voiceMsg;
+  num = +voiceMsg;
 
   // Check if valid number
   if (Number.isNaN(num)) {
@@ -46,21 +56,41 @@ function checkNumber(voiceMsg) {
 
   // Check in range
   if (num > 100 || num < 1) {
-    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    msgEl.innerHTML +=
+      "<div class='alert-box'>Number must be between 1 and 20</div>";
     return;
   }
 
   // Check number
   if (num === secretNumber) {
-    document.body.innerHTML = `
-        <h2>Congrats! You have guessed the number! <br><br>
+    document.body.style.backgroundColor = "#60b347";
+    numberEl.innerHTML = `${num}`;
+    numberEl.style.width = "30rem";
+    articleEl.innerHTML = `
+        <h2 class="congrats">Congrats! You have guessed the number! <br><br>
         It was ${num}</h2>
-        <button class="play-again" id="play-again">Play Again</button>
+        <button class="play-again btn" id="play-again">Play Again</button>
       `;
-  } else if (num > secretNumber) {
-    msgEl.innerHTML += "<div>GO LOWER</div>";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highScore").textContent = highScore;
+    }
+  } else if (num !== secretNumber) {
+    if (score > 1) {
+      if (num > secretNumber) {
+        msgEl.innerHTML += "<div class='alert-box'> GO LOWER </div>";
+      }
+      score--;
+      scoreEl.textContent = score;
+    }
   } else {
-    msgEl.innerHTML += "<div>GO HIGHER</div>";
+    if (score > 1) {
+      if (num < secretNumber) {
+        msgEl.innerHTML += "<div class='alert-box'> GO HIGHER </div>";
+      }
+      score--;
+      scoreEl.textContent = score;
+    }
   }
 }
 
@@ -71,116 +101,89 @@ recognition.addEventListener("result", onSpeak);
 recognition.addEventListener("end", () => recognition.start());
 
 document.body.addEventListener("click", (e) => {
-  if (e.target.id == "play-again") {
+  if (e.target.id === "play-again") {
     window.location.reload();
   }
 });
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-let secretNumber = Math.trunc(Math.random() * 2) + 1;
-console.log(secretNumber);
-
-let score = 20;
-let highscore = 0;
 
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
 
-// document.querySelector(".check").addEventListener("click", function () {
-//   const guess = Number(document.querySelector(".guess").value);
-//   console.log(guess, typeof guess);
+voiceBtn.addEventListener("click", () => {
+  // Start recognition and game
+  recognition.start();
 
-//   // When there is no input
-//   if (!guess) {
-//     // document.querySelector('.message').textContent = '⛔️ No number!';
-//     displayMessage("⛔️ No number!");
+  containerEl.style.display = "flex";
+  articleEl.style.display = "flex";
+  btnContainer.style.display = "none";
+  leftEl.style.display = "none";
+});
 
-//     // When player wins
-//   } else if (guess === secretNumber) {
-//     // document.querySelector('.message').textContent = '🎉 Correct Number!';
-//     displayMessage("🎉 Correct Number!");
-//     document.querySelector(".number").textContent = secretNumber;
+manualBtn.addEventListener("click", () => {
+  containerEl.style.display = "flex";
+  btnContainer.style.display = "none";
+});
 
-//     document.querySelector("body").style.backgroundColor = "#60b347";
-//     document.querySelector(".number").style.width = "30rem";
+const guessEl = document.querySelector(".guess");
+guessEl.addEventListener("keyup", myFunction);
 
-//     if (score > highscore) {
-//       highscore = score;
-//       document.querySelector(".highscore").textContent = highscore;
-//     }
+const checkEl = document.querySelector(".check");
+checkEl.addEventListener("click", myFunction);
 
-//     // When guess is wrong
-//   } else if (guess !== secretNumber) {
-//     if (score > 1) {
-//       // document.querySelector('.message').textContent =
-//       // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
-//       displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
-//       score--;
-//       document.querySelector(".score").textContent = score;
-//     } else {
-//       // document.querySelector('.message').textContent = '💥 You lost the game!';
-//       displayMessage("💥 You lost the game!");
-//       document.querySelector(".score").textContent = 0;
-//     }
-//   }
+function myFunction(event) {
+  if (event.key === "Enter" || event.target.className === "btn check") {
+    console.log(event);
 
-//   //   // When guess is too high
-//   // } else if (guess > secretNumber) {
-//   //   if (score > 1) {
-//   //     document.querySelector('.message').textContent = '📈 Too high!';
-//   //     score--;
-//   //     document.querySelector('.score').textContent = score;
-//   //   } else {
-//   //     document.querySelector('.message').textContent = '💥 You lost the game!';
-//   //     document.querySelector('.score').textContent = 0;
-//   //   }
+    const guess = Number(document.querySelector(".guess").value);
+    // const guess = document.querySelector(".guess").value;
 
-//   //   // When guess is too low
-//   // } else if (guess < secretNumber) {
-//   //   if (score > 1) {
-//   //     document.querySelector('.message').textContent = '📉 Too low!';
-//   //     score--;
-//   //     document.querySelector('.score').textContent = score;
-//   //   } else {
-//   //     document.querySelector('.message').textContent = '💥 You lost the game!';
-//   //     document.querySelector('.score').textContent = 0;
-//   //   }
-//   // }
-// });
+    console.log(guess, typeof guess);
 
-// document.querySelector(".again").addEventListener("click", function () {
-//   score = 20;
-//   secretNumber = Math.trunc(Math.random() * 20) + 1;
+    // When there is no input
+    if (!guess) {
+      // document.querySelector('.message').textContent = '⛔️ No number!';
+      displayMessage("⛔️ No number!");
 
-//   // document.querySelector('.message').textContent = 'Start guessing...';
-//   displayMessage("Start guessing...");
-//   document.querySelector(".score").textContent = score;
-//   document.querySelector(".number").textContent = "?";
-//   document.querySelector(".guess").value = "";
+      // When player wins
+    } else if (guess === secretNumber) {
+      // document.querySelector('.message').textContent = '🎉 Correct Number!';
+      console.log("you won");
+      displayMessage("🎉 Correct Number!");
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
 
-//   document.querySelector("body").style.backgroundColor = "#222";
-//   document.querySelector(".number").style.width = "15rem";
-// });
+      checkEl.style.display = "none";
+      btnAgainManualEl.style.display = "initial";
+      btnAgainManualEl.addEventListener("click", () => {
+        console.log("Top again btn clicked");
+        window.location.reload();
+      });
 
-///////////////////////////////////////
-// Coding Challenge #1
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector(".highScore").textContent = highScore;
+      }
 
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
+      // When guess is wrong
+    } else if (guess !== secretNumber) {
+      if (score > 1) {
+        displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+        score--;
+        scoreEl.textContent = score;
+      } else {
+        // document.querySelector('.message').textContent = '💥 You lost the game!';
+        displayMessage("💥 You lost the game!");
+        scoreEl.textContent = "";
 
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-GOOD LUCK 😀
-*/
+        checkEl.style.display = "none";
+        btnAgainManualEl.style.display = "initial";
+        btnAgainManualEl.addEventListener("click", () => {
+          console.log("Top again btn clicked");
+          window.location.reload();
+        });
+      }
+    }
+  }
+}
